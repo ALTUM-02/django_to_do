ac7b6b67d90dd23a890a1b4826cf637d1c0b9a4d
ALTUM-02 <martin249jesus@gmail.com>
2026-01-26 14:35:26 +0300

    new change

--- todo/settings.py
+++ todo/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL ='/media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /  'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
