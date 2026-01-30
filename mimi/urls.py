0c3cf912af7cbcb548ea84eace00f1774826155f
ALTUM-02 <martin249jesus@gmail.com>
2026-01-30 12:35:13 +0300

    new change

--- mimi/urls.py
+++ mimi/urls.py
@@ -3,16 +3,16 @@ from .views import *
 from . import views
 
 urlpatterns = [
-    path('home/', home, name='home'),
-    path('register/', register, name='register'),
+    path('home/',views.home, name='home'),
+    path('register/',views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),  
 
     path('tasks/<int:id>/', views.task_detail, name='task_detail'),
     path("task/", views.task_list, name="task_list"),
     path("task_add/",task_add, name="task_add"),
-    path("task_delete/<int:task_id>", task_delete, name="task_delete"), 
-    path("task_update/<int:task_id>",task_update, name="task_update"), 
+    path("task_delete/<int:task_id>",views.task_delete, name="task_delete"), 
+    path("task_update/<int:task_id>",views.task_update, name="task_update"), 
 
 
 
